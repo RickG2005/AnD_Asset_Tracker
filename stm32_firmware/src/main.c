d964651e4b1fb190712a15c665a7d1d19923ff5c
Rick Gonsalves <rick2005.g@gmail.com>
2026-07-01 19:49:53 +0530

    feat: blueprint for engine fail interrupt & copy of latest location data

--- stm32_firmware/src/main.c
+++ stm32_firmware/src/main.c
@@ -23,56 +23,74 @@ __attribute__((naked)) void Reset_Handler(void) {
     );
 }
 
-void Run_TelemetryDisplayTask(void) {
 
-    struct VehicleDataPacket tracking_data;
+
+
+volatile struct VehicleDataPacket latest_telemetry_data; //in case of interrupt
+
+static uint8_t curr_enginestatus1 = 1;
+static uint8_t curr_enginestatus2 = 1;
+
+void Run_LocationDisplayTask(struct VehicleDataPacket *tracking_data) {
+
     char text_buffer[20];
 
     LCD_Clear();
-    LCD_PrintString("Waiting...");
 
-    while(1){
+    //Display Latitude
+    long lat_deg = tracking_data->latitude / 1000000;
+    long lat_frac = tracking_data->latitude % 1000000;
+    if (lat_frac < 0) lat_frac = -lat_frac;
 
-        if (UART1_CheckForPacket(&tracking_data) == 1){
+    snprintf(text_buffer, sizeof(text_buffer), "Lat:%ld.%06ld", lat_deg, lat_frac);
+    LCD_PrintString(text_buffer);
 
-            LCD_Clear();
+    LCD_SendCommand(0xC0);
 
-            //Display Latitude
-            long lat_deg = tracking_data.latitude / 1000000;
-            long lat_frac = tracking_data.latitude % 1000000;
-            if (lat_frac < 0) lat_frac = -lat_frac;
+    //Display Longitude
+    long lon_deg = tracking_data->longitude / 1000000;
+    long lon_frac = tracking_data->longitude % 1000000;
+    if (lon_frac < 0) lon_frac = -lon_frac;
+    
+    snprintf(text_buffer, sizeof(text_buffer), "Lon:%ld.%06ld", lon_deg, lon_frac);
+    LCD_PrintString(text_buffer);
 
-            snprintf(text_buffer, sizeof(text_buffer), "Lat:%ld.%06ld", lat_deg, lat_frac);
-            LCD_PrintString(text_buffer);
+    LED_Toggle();
+}
 
-            LCD_SendCommand(0xC0);
 
-            //Display Longitude
-            long lon_deg = tracking_data.longitude / 1000000;
-            long lon_frac = tracking_data.longitude % 1000000;
-            if (lon_frac < 0) lon_frac = -lon_frac;
-            
-            snprintf(text_buffer, sizeof(text_buffer), "Lon:%ld.%06ld", lon_deg, lon_frac);
-            LCD_PrintString(text_buffer);
+void CheckEngineStatus(struct VehicleDataPacket *tracking_data){
 
-            LED_Toggle();
-
-        }
+    uint8_t inc_enginestatus1 = (tracking_data->status) & (1<<1);
+    uint8_t inc_enginestatus2 = (tracking_data->status) & (1<<2);
+    if (inc_enginestatus1 == 0 || inc_enginestatus2 == 0){
+        //trigger interrupt
+        curr_enginestatus1 = inc_enginestatus1;
+        curr_enginestatus2 = inc_enginestatus2;
     }
+    
+
 }
 
 int main (void) {
-    extern uint8_t rx_index;
-    extern uint8_t rx_state;
-    rx_index = 0;
-    rx_state = 0;
+
+    struct VehicleDataPacket live_packet;
 
     UART1_Init();
     LCD_Init();
     LED_Status_Init();
+    LCD_Clear();
+    LCD_PrintString("Starting...");
 
-    Run_TelemetryDisplayTask();
 
-    while(1);
+    while(1){
+        if (UART1_CheckForPacket(&live_packet) == 1){
+
+            latest_telemetry_data = live_packet;
+
+            CheckEngineStatus(&live_packet);
+            Run_LocationDisplayTask(&live_packet);
+        }
+    }
 
 }
